87e92bd413ed82732b9dd1c6b8d731a597f47f10
الكنج حمودي الاسطوره <mogsnfknsgqns@gmail.com>
2024-08-25 21:56:24 +0300

    Create yejp.py

---
+++ yejp.py
@@ -0,0 +1,64 @@
+import sys
+import urllib.request
+import urllib.error
+import traceback
+
+	    
+          
+# Functions for colored console output
+def print_red(text):
+    print("\033[91m {}\033[00m" .format(text))
+
+	    
+          
+def print_green(text):
+    print("\033[92m {}\033[00m" .format(text))
+
+	    
+          
+def print_yellow(text):
+    print("\033[93m {}\033[00m" .format(text))
+
+	    
+          
+def print_light_purple(text):
+    print("\033[94m {}\033[00m" .format(text))
+
+	    
+          
+def print_purple(text):
+    print("\033[95m {}\033[00m" .format(text))
+
+	    
+          
+def print_cyan(text):
+    print("\033[96m {}\033[00m" .format(text))
+
+	    
+          
+def print_light_gray(text):
+    print("\033[97m {}\033[00m" .format(text))
+
+	    
+          
+def print_black(text):
+    print("\033[98m {}\033[00m" .format(text))
+
+	    
+          
+# Printing decorative lines and messages
+print_yellow("  ———————————")
+print("")
+print("")
+print_green("          …………………")
+print_purple("   .                .            .")
+print_cyan("           الكنج حمودي الاسطورة  ")
+print("")
+print("         .                   .")
+print("                 .                      .")
+print("")
+print_light_gray("   .            .")
+print_light_purple("          …………………")
+print_yellow("————————————-")
+
+	 
